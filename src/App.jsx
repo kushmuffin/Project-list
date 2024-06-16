0fae00284c1de9eb3b1bb5cb7b36e259bc86b261
kushmuffin <kushlee24@gmail.com>
2024-06-17 01:58:28 +0800

    完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,125 @@
 import { useState } from "react";
+
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
-  const [projectState, setProjectState] = useState({
-    // currentAction: 'nothing-selected',
+  const [projectsState, setprojectsState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: [],
   });
-
+  //添加task
+  function handleAddTask(text) {
+    setprojectsState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks],
+      };
+    });
+  }
+  //刪除task
+  function handleDeleteTask(id) {
+    setprojectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
+  //選取側選單project
+  function handleSelectProject(id) {
+    setprojectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
+  //添加新project
   function handleStartAddProject() {
-    setProjectState(prevState => {
+    setprojectsState((prevState) => {
       return {
         ...prevState,
         selectedProjectId: null,
-      }
+      };
+    });
+  }
+  //返回主畫面並清空輸入
+  function handleCancelAddProject() {
+    setprojectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+  //添加新project
+  function handleAddProject(projectData) {
+    setprojectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+  //刪除project
+  function handleDeleteProject() {
+    setprojectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
     });
   }
 
-  function handleAddProject(projectData) {
-    setProjectState(prevState => {
-      const newProject = {
-        ...projectData,
-        id: Math.random()
-      }
-      return {
-        ...prevState,
-        projects: [...prevState.preject, newProject]
-      }
-    })
-  }
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
 
-  console.log(projectState)
+  let content = (
+    <SelectedProject
+      project={selectedProject}
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
+  );
 
-  let content;
-
-  if (projectState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />
-  } else if (projectState.selectedProjectId === undefined) {
-    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  if (projectsState.selectedProjectId === null) {
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
+      />
       {content}
     </main>
   );
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,14 +1,22 @@
-import noProjectImage from "../assets/no-projects.png"
-import Button from "./Button.jsx"
+import noProjectImage from "../assets/no-projects.png";
+import Button from "./Button.jsx";
 export default function NoProjectSelected({ onStartAddProject }) {
   return (
     <div className="mt-24 text-center w-2/3">
-      <img src={noProjectImage} className="w-16 h-16 object-contain mx-auto" alt="A empty task list" />
-      <h2 className="text=xl font-bold text stroke-stone-500 mt-4 my-4">No Project Selected</h2>
-      <p className="text-stone-400 mb-4">Select a project or get started with a new one</p>
+      <img
+        src={noProjectImage}
+        className="w-16 h-16 object-contain mx-auto"
+        alt="A empty task list"
+      />
+      <h2 className="text=xl font-bold text stroke-stone-500 mt-4 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Select a project or get started with a new one
+      </p>
       <p className="mt-8">
         <Button onClick={onStartAddProject}>Create new project</Button>
       </p>
     </div>
-  )
+  );
 }
